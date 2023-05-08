fa15b90e35cfeada7443684478fcfad23a4114d6
Arun Singh <57107878+arunsingh-me@users.noreply.github.com>
2023-05-08 10:53:57 +0530

    resolved deps

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom';
-import  Container  from './Container';
-import SearchInput from './SearchInput';
-import UserMenu from './UserMenu';
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import SearchInput from "./SearchInput";
+import UserMenu from "./UserMenu";
 // import SearchIcon from '@mui/icons-material/Search';
 // import LanguageIcon from '@mui/icons-material/Language';
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 // import {Avatar} from '@material-ui/core';
 function Header() {
   function currentUser() {
-    return null
+    return null;
   }
   return (
-    <div className='header fixed w-full bg-white z-10 shadow-sm '>
-    <Link to='/'>
-    <img src="https://dynamic.brandcrowd.com/asset/logo/f26fbab6-d2ac-4944-bc94-09ddf7cde5a2/logo-search-grid-1x?v=636989397566000000&text=Home+stays" alt="HomeStay" className='header__icon hidden md:block cursor-pointer' />
-    </Link>
-   <div className="py-4 border-b-[1px]">
-   {/* <Container anything/> */}
-   <Container>
-    <div className='flex flex-row item-center justify-between gap-3 md:gap-0'>
-    <SearchInput/>
-    <UserMenu currentUser ={currentUser}/>
+    <div className="header fixed w-full bg-white z-10 shadow-sm ">
+      <Link to="/">
+        <img
+          src="https://dynamic.brandcrowd.com/asset/logo/f26fbab6-d2ac-4944-bc94-09ddf7cde5a2/logo-search-grid-1x?v=636989397566000000&text=Home+stays"
+          alt="HomeStay"
+          className="header__icon hidden md:block cursor-pointer"
+        />
+      </Link>
+      <div className="py-4 border-b-[1px]">
+        {/* <Container anything/> */}
+        <Container>
+          <div className="flex flex-row item-center justify-between gap-3 md:gap-0">
+            <SearchInput />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      Hi lol
     </div>
-   </Container>
-    
-   </div>
-     
-     
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/RegisterModel.js
+++ src/RegisterModel.js
@@ -1,125 +1,131 @@
-import React from 'react'
-import axios from 'axios'
-import { AiFillGithub } from 'react-icons/ai'
-import {FcGoogle} from 'react-icons/fc'
-import { useCallback ,useState } from 'react'
-import { useForm} from 'react-hook-form'
-import useRegisterModel from './hooks/useRegisterModel'
-import Modal  from './Modal'
-import Heading  from './Heading'
-import Input  from './Input'
-import { toast } from 'react-hot-toast'
-import Buttons from './Buttons'
-import useLoginModel from './hooks/useLoginModel'
+import React from "react";
+import axios from "axios";
+// import { AiFillGithub } from "react-icons/ai";
+// import { FcGoogle } from "react-icons/fc";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import useRegisterModel from "./hooks/useRegisterModel";
+import Modal from "./Modal";
+import Heading from "./Heading";
+import Input from "./Input";
+import { toast } from "react-hot-toast";
+import Buttons from "./Buttons";
+import useLoginModel from "./hooks/useLoginModel";
 
 const RegisterModel = () => {
-    const registerModel =useRegisterModel()
-    const loginModel =useLoginModel()
-    const[isLoading ,setIsLoading] =useState(false)
-    const {
-      register,
-      handleSubmit,
-      formState :{
-        errors,
-      }
-    } =useForm({
-      defaultValues :{
-        name : '',
-        email : '',
-        password: ''
-      }
-    })
+  const registerModel = useRegisterModel();
+  const loginModel = useLoginModel();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-  const onsubmit =(data)=>{
-    // console.log('haidi')
-    setIsLoading(true)
-    axios.post('/api/register',data)
-    .then(()=>{
-      registerModel.onClose()
-    })
-    .catch(
-      toast.error("Something went wrong")
-    )
-    .finally(()=>{
-      setIsLoading(false)
-    })
-
-  }
-  const bodyContent =(
-    <div className='flex flex-col gap-4'>
+  const onsubmit = async (data) => {
+    try {
+      setIsLoading(true);
+      // write it in fetch
+      const res = await fetch("http://localhost:3002/user/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      const result = await res.json();
+      console.log(result);
+      if (res.ok) {
+        toast.success("Account created");
+        registerModel.onClose();
+      } else {
+        toast.error("Something went wrong");
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
       <Heading title="Welcome to HomeStay" subtitle="Create an account" />
       {/* this is input field for form */}
-     
-       <Input 
+
+      <Input
         id="name"
         label="Name"
         disabled={isLoading}
         register={register}
         errors={errors}
         required
-      /> 
-       <Input 
+      />
+      <Input
         id="email"
         label="Email"
         disabled={isLoading}
         register={register}
         errors={errors}
         required
-      /> 
-       <Input 
+      />
+      <Input
         id="password"
         label="Password"
-        type='password'
+        type="password"
         disabled={isLoading}
         register={register}
         errors={errors}
         required
-      /> 
+      />
     </div>
-  )
+  );
 
-  const footerContent =(
-    <div className='flex flex-col gap-4 mt-3'>
-      <Buttons 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      {/* <Buttons
         outline
         label="Continue with Google"
         icons={FcGoogle}
-        onClick={()=>{}}
+        onClick={() => {}}
       />
-       <Buttons 
+      <Buttons
         outline
         label="Continue with Github"
         icons={AiFillGithub}
-        onClick={()=>{}}
-      />
-      <div className='text-neutral-500 text-center mt-1 mb-1 font-light '>
-        <div className='flex flex-row items-center gap-2 justify-center'>
-        <div>
-          Already a User?
+        onClick={() => {}}
+      /> */}
+      <div className="text-neutral-500 text-center mt-1 mb-1 font-light ">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already a User?</div>
+          <div
+            className="cursor-pointer hover:underline text-neutral-800"
+            onClick={loginModel.onOpen}
+          >
+            Log in
+          </div>
         </div>
-        <div className='cursor-pointer hover:underline text-neutral-800' onClick={loginModel.onOpen}>
-          Log in
-        </div>
-
-        </div>
-
       </div>
     </div>
-  )
+  );
   return (
     <>
-    <Modal 
-      disabled={isLoading}
-      isOpen={registerModel.isOpen}
-      title='Register'
-      actionLabel='Continue'
-      onClose={registerModel.onClose}
-      onSubmit={handleSubmit(onsubmit)}
-      body={bodyContent}
-      footer={footerContent}
-    />
+      <Modal
+        disabled={isLoading}
+        isOpen={registerModel.isOpen}
+        title="Register"
+        actionLabel="Continue"
+        onClose={registerModel.onClose}
+        onSubmit={handleSubmit(onsubmit)}
+        body={bodyContent}
+        footer={footerContent}
+      />
     </>
-  )
-}
+  );
+};
 
-export default RegisterModel
+export default RegisterModel;
