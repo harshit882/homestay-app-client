eb43aa9400ebec627d9e0b8e91464929f9ad20f3
Arun Singh <57107878+arunsingh-me@users.noreply.github.com>
2023-05-11 16:17:56 +0530

    upload api fix

--- src/pages/PlacesAccomdation.js
+++ src/pages/PlacesAccomdation.js
@@ -3,50 +3,59 @@ import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
 import axios from "axios";
 
-
 const PlacesAccomdation = () => {
-    // variables to manage state of a form
-    const [title ,setTitle] =useState('')
-    const [address ,setAddress] =useState('')
-    const [addedPhotos ,setAddedPhotos] =useState([])
-    const [photoLink ,setPhotoLink] =useState('')
-    const [description ,setDescription] =useState('')
-    const [perks, setPerks] =useState([])
-    const [extraInfo, setExtraInfo] =useState('')
-    const [checkIn , setCheckIn] =useState('')
-    const [checkOut , setCheckOut] =useState('')
-    const [MaxGuests , setMaxGuests] =useState(1)
+  // variables to manage state of a form
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [MaxGuests, setMaxGuests] = useState(1);
 
-    
   const { action } = useParams();
-    async function addPhotoByInputLink(e) {
-      e.preventDefault()
-      const {data:filename}=await axios.post('https://homestay-app-server.cyclic.app/upload', {link:photoLink})
-      setAddedPhotos((previous)=>{
-       return [...previous, filename]
-      })
-      setPhotoLink('')
+  async function addPhotoByInputLink(e) {
+    e.preventDefault();
+    const { data: filename } = await axios.post(
+      "https://homestay-app-server.cyclic.app/upload-by-link",
+      { link: photoLink }
+    );
+    setAddedPhotos((previous) => {
+      return [...previous, filename];
+    });
+    setPhotoLink("");
+  }
+
+  async function uploadByLaptop(e) {
+    const files = e.target.files;
+    console.log({ files });
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append("photos", files[i]);
     }
 
-    function uploadByLaptop(e) {
-      const files = e.target.files
-      console.log({files})
-      const data =new FormData()
-      for(let i=0 ; i < files.length; i++) {
-        data.append('photos',files[i])
+    const { data: filenames } = await axios.post(
+      "https://homestay-app-server.cyclic.app/upload",
+      data,
+      {
+        headers: { "Content-type": "multipart/form-data" },
       }
-      
-      axios.post('https://homestay-app-server.cyclic.app/upload', data, {
-        headers:{'Content-type': 'multipart/form-data'}
-      } )
-      .then((response)=>{
-        const {data:filenames} =response
-        setAddedPhotos((previous)=>{
-          return [...previous, ...filenames]
-         })
-        // console.log(data)
-      })
-    }
+    );
+
+    setAddedPhotos((prev) => {
+      return [...prev, ...filenames];
+    });
+
+    // .then((response) => {
+    //   const { data: filenames } = response;
+    //   setAddedPhotos((previous) => {
+    //     return [...previous, ...filenames];
+    //   });
+    // console.log(data)
+  }
   return (
     <>
       {action !== "new" && (
@@ -90,7 +99,7 @@ const PlacesAccomdation = () => {
                   id="grid-first-name"
                   type="text"
                   value={title}
-                  onChange={e=>setTitle(e.target.value)}
+                  onChange={(e) => setTitle(e.target.value)}
                   placeholder="title, for example : My lovelu apt"
                 />
               </div>
@@ -106,7 +115,7 @@ const PlacesAccomdation = () => {
                   id="grid-last-name"
                   type="text"
                   value={address}
-                  onChange={e=>setAddress(e.target.value)}
+                  onChange={(e) => setAddress(e.target.value)}
                   placeholder="address"
                 />
               </div>
@@ -127,128 +136,144 @@ const PlacesAccomdation = () => {
                     className="appearance-none block bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white flex-grow"
                     placeholder={"Add using a link ....jpg"}
                     value={photoLink}
-                    onChange={e=>setPhotoLink(e.target.value)}
+                    onChange={(e) => setPhotoLink(e.target.value)}
                   />
-                  <button onClick={addPhotoByInputLink} className="bg-red-400 text-white rounded-md h-full py-3 px-4 mb-3">
+                  <button
+                    onClick={addPhotoByInputLink}
+                    className="bg-red-400 text-white rounded-md h-full py-3 px-4 mb-3"
+                  >
                     Add photo
                   </button>
                 </div>
                 <div className=" grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-6 mt-2 ">
-                {addedPhotos.length > 0 && addedPhotos.map((link)=>{
-                  <div>
-                    {/* {link} */}
-                    <img className="rounded-2xl" src={''} alt="" />
-                  </div>
-                })}
-              
-                <label className="w-2/6 cursor-pointer border bg-white rounded-2xl text-xl p-4 items-center flex gap-1">
-                <input type="file" className="hidden" multiple onChange={uploadByLaptop} />
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8    h-8 mt-0.75">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                </svg>
+                  {addedPhotos.length > 0 &&
+                    addedPhotos.map((link) => {
+                      <div>
+                        {/* {link} */}
+                        <img className="rounded-2xl" src={""} alt="" />
+                      </div>;
+                    })}
 
+                  <label className="w-2/6 cursor-pointer border bg-white rounded-2xl text-xl p-4 items-center flex gap-1">
+                    <input
+                      type="file"
+                      className="hidden"
+                      multiple
+                      onChange={uploadByLaptop}
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-8    h-8 mt-0.75"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                      />
+                    </svg>
                     Upload
                   </label>
                 </div>
                 <div className="w-full  px-3 mb-6 md:mb-0 -mx-3 mt-6">
-                <label
-                  className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                  for="grid-first-name"
-                >
-                  Description
-                  {/* <p className="text-light">tell more about your place</p> */}
-                </label>
-                
+                  <label
+                    className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                    for="grid-first-name"
+                  >
+                    Description
+                    {/* <p className="text-light">tell more about your place</p> */}
+                  </label>
 
-                <textarea
-                  className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rouded-md"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="tell more about your place"
-                  value={description}
-                  onChange={e=>setDescription(e.target.value)}
-                  cols={60}
-                  rows={5}
-                />
-              </div>
+                  <textarea
+                    className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rouded-md"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="tell more about your place"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    cols={60}
+                    rows={5}
+                  />
+                </div>
 
-            <Perks selected={perks} onchange={setPerks}/>
-              <div className="w-full  px-3 mb-6 md:mb-0 -mx-3 mt-6">
-                <label
-                  className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                  for="grid-first-name"
-                >
-                  Extra Info
-                  {/* <p className="text-light">tell more about your place</p> */}
-                </label>
-                
+                <Perks selected={perks} onchange={setPerks} />
+                <div className="w-full  px-3 mb-6 md:mb-0 -mx-3 mt-6">
+                  <label
+                    className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                    for="grid-first-name"
+                  >
+                    Extra Info
+                    {/* <p className="text-light">tell more about your place</p> */}
+                  </label>
 
-                <textarea
-                  className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-md"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="for eg. house rules and regulations"
-                  value={extraInfo}
-                  onChange={e=>setExtraInfo(e.target.value)}
-                  cols={60}
-                  rows={2}
-                />
-              </div>
-              <div className="w-full  px-3 mb-6 md:mb-0 -mx-3 mt-6">
-                <label
-                  className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                  for="grid-first-name"
-                >
-                  Check in&out time
-                 
-                </label>
-                 <p className="text-cyan-600 font-serif font-thin">add check in&out time ,remember to have some time window for room cleaning</p>
-                <div className="grid sm:grid-cols-3 mt-2 gap-2">
-                <div className="-mb-1">
-                <p className=" font-serif font-thin">Check in time</p>
-                <input
-                  className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="eg. 12:00 pm"
-                  value={checkIn}
-                  onChange={e=>setCheckIn(e.target.value)}
-                
-                />
+                  <textarea
+                    className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-md"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="for eg. house rules and regulations"
+                    value={extraInfo}
+                    onChange={(e) => setExtraInfo(e.target.value)}
+                    cols={60}
+                    rows={2}
+                  />
                 </div>
-                <div className="-mb-1">
-                <p className=" font-serif font-thin">Check out time</p>
-                <input
-                  className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="eg. 11:00 am"
-                  value={checkOut}
-                  onChange={e=>setCheckOut(e.target.value)}
-                  
-                />
+                <div className="w-full  px-3 mb-6 md:mb-0 -mx-3 mt-6">
+                  <label
+                    className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                    for="grid-first-name"
+                  >
+                    Check in&out time
+                  </label>
+                  <p className="text-cyan-600 font-serif font-thin">
+                    add check in&out time ,remember to have some time window for
+                    room cleaning
+                  </p>
+                  <div className="grid sm:grid-cols-3 mt-2 gap-2">
+                    <div className="-mb-1">
+                      <p className=" font-serif font-thin">Check in time</p>
+                      <input
+                        className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
+                        id="grid-first-name"
+                        type="text"
+                        placeholder="eg. 12:00 pm"
+                        value={checkIn}
+                        onChange={(e) => setCheckIn(e.target.value)}
+                      />
+                    </div>
+                    <div className="-mb-1">
+                      <p className=" font-serif font-thin">Check out time</p>
+                      <input
+                        className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
+                        id="grid-first-name"
+                        type="text"
+                        placeholder="eg. 11:00 am"
+                        value={checkOut}
+                        onChange={(e) => setCheckOut(e.target.value)}
+                      />
+                    </div>
+                    <div className="-mb-1">
+                      <p className=" font-serif font-thin">Max guests</p>
+                      <input
+                        className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
+                        id="grid-first-name "
+                        type="number"
+                        max={4}
+                        min={1}
+                        value={MaxGuests}
+                        onChange={(e) => setMaxGuests(e.target.value)}
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="-mb-1">
-                <p className=" font-serif font-thin">Max guests</p>
-                <input
-                  className="appearance-none block w-full bg-white text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white rounded-2xl"
-                  id="grid-first-name "
-                  type="number"
-                  max={4}
-                  min={1}
-                  value={MaxGuests}
-                  onChange={e=>setMaxGuests(e.target.value)}
-                />
-                </div>
-                </div>
-             
-              </div>
               </div>
             </div>
-     
+
             <button className="w-full rounded-full bg-red-400 text-white h-full py-3 px-4 mb-3">
-                    Save 
-                  </button>
+              Save
+            </button>
           </form>
         </div>
       )}
