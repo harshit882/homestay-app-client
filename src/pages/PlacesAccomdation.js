c47835d1c97ab3d0f835f6c8c13a25541e05368f
Arun Singh <57107878+arunsingh-me@users.noreply.github.com>
2023-05-27 23:03:54 +0530

    Added Accomodation Page

--- src/pages/PlacesAccomdation.js
+++ src/pages/PlacesAccomdation.js
@@ -1,44 +1,44 @@
 import { useState } from "react";
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 import PlacesForm from "./PlacesForm";
-
+import PlacesPage from "./PlacesPage";
 const PlacesAccomdation = () => {
   // variables to manage state of a form
- 
-  const [redirectToAccommodation ,setRedirectToAccommodation] =useState(false)
+
+  const [redirectToAccommodation, setRedirectToAccommodation] = useState(false);
 
   const { action } = useParams();
-
 
   return (
     <>
       {action !== "new" && (
-        <div className="my-4  text-center">
-          <Link
-            className="inline-flex bg-red-400 text-white rounded-full px-6 py-2 "
-            to={"/account/accommodation/new"}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+        <>
+          <PlacesPage />
+          <div className="my-4  text-center">
+            <Link
+              className="inline-flex bg-red-400 text-white rounded-full px-6 py-2 "
+              to={"/account/accommodation/new"}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-            Add new place
-          </Link>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+              Add new place
+            </Link>
+          </div>
+        </>
       )}
-      {action === "new" && (
-       <PlacesForm/>
-      )}
+      {action === "new" && <PlacesForm />}
     </>
   );
 };
